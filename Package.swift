175e1245dc1b72e4136666b3c8788d2486d1dc15
Bryan Lloyd Anderson <bryan@dvlop.io>
2020-09-02 15:33:03 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "BAData",
-    platforms: [.iOS(.v12), .macOS(.v10_15)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -15,15 +15,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/bryan1anderson/BAUtilities", .branch("master")),
-    .package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.0.0"),
-    .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3"),
-    .package(url: "https://github.com/kean/Nuke", from: "9.1.1"),
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("develop")),
-    .package(url: "https://github.com/utahiosmac/Marshal", from: "1.2.8"),
-    .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.0"),
+        .package(url: "https://github.com/bryan1anderson/BAUtilities", .branch("master")),
+        .package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3"),
+        .package(url: "https://github.com/kean/Nuke", from: "9.1.1"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("develop")),
+        .package(url: "https://github.com/utahiosmac/Marshal", from: "1.2.8"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.0"),
         .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.0.0"),
-        .package(url: "https://github.com/kean/FetchImage", from: "0.2.1")
+        .package(url: "https://github.com/kean/FetchImage", from: "0.2.1"),
+        .package(url: "https://github.com/thoughtbot/CombineViewModel", from: "0.3.1"),
+
         
     ],
     targets: [
@@ -31,7 +33,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BAData",
-            dependencies: ["PhoneNumberKit", "Alamofire", "Nuke", "RxSwift", "RxCocoa", "Marshal", "BAUtilities", "SwiftyJSON", "Reachability"]),
+            dependencies: ["PhoneNumberKit", "Alamofire", "Nuke", "RxSwift", "RxCocoa", "Marshal", "BAUtilities", "SwiftyJSON", "Reachability", "CombineViewModel", "FetchImage"]),
         .testTarget(
             name: "BADataTests",
             dependencies: ["BAData"]),
